c618ec609ea9802152bc35d6c582b8a7f4d8d507
Juan Couso <juan.couso@gmail.com>
2017-09-12 18:22:57 -0300

    banner done

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,10 +1,12 @@
 class PropertiesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :index]
+  layout 'landing-page', only: [:home]
 
   def index
   end
 
   def home
+    @propeties = Property.all
   end
 
 end
