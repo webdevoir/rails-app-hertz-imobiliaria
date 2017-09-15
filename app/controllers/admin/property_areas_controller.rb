6bc22ab7149d0255de2780b6485fd1818c81e58d
Juan Couso <juan.couso@gmail.com>
2017-09-14 22:51:33 -0300

    finish create Property Area backend

--- app/controllers/admin/property_areas_controller.rb
+++ app/controllers/admin/property_areas_controller.rb
@@ -6,5 +6,21 @@ class Admin::PropertyAreasController < ApplicationController
   end
 
   def create
+    @property_area = PropertyArea.new(property_area_params)
+    @landlord = Landlord.find(params[:landlord_id])
+    @property = @landlord.properties.find(params[:property_id])
+    @property_area.property = @property
+    if @property.save
+      redirect_to new_admin_landlord_property_property_values_path(@landlord, @property )
+    else
+      render :new
+    else
+
+  end
+
+  private
+
+  def property_area_params
+    params.require(:property_area).permit(:scale, :total_area, :usable_area, :area_land)
   end
 end
