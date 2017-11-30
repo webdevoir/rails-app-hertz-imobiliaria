f60bccb8fe9faac80a26af00a8acb2186b4a37d1
Juan Couso <juan.couso@gmail.com>
2017-11-30 01:13:17 -0200

    initial seed, still work needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,59 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+Landlord.destroy_all
+
+Property.destroy_all
+
+User.destroy_all
+
 User.create(email:"test@test.com", password: "123123")
+
+
+landlord1 = [
+  {
+
+
+    name: "Juan Manuel Couso"
+    rg: "V317245-U"
+    cpg: "231.311.748-03"
+    birthday: Date.new(1986,8,19)
+    martial_status: "Solteiro"
+    profession: "Engenheiro"
+    phone: "11994325759"
+    email: "juan.couso@gmail.com"
+    address:
+    cep:
+    deposit:
+    adm_rate:
+    cep:
+    deposit:
+    first_rate:
+    type:
+    bank:
+    agency:
+    number:
+    user_id:
+      t.string   "name"
+    t.string   "rg"
+    t.string   "cpf"
+    t.date     "birthday"
+    t.string   "martial_status"
+    t.string   "profession"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "address"
+    t.string   "cep"
+    t.boolean  "deposit"
+    t.decimal  "adm_rate"
+    t.decimal  "first_rate"
+    t.string   "type"
+    t.string   "bank"
+    t.string   "agency"
+    t.string   "number"
+    t.integer  "user_id"
+
+  }
+]
 
