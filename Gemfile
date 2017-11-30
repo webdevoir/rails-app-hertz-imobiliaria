f1a6b56affeb12566e23097cf0e1201b78857886
Juan Couso <juan.couso@gmail.com>
2017-11-30 15:16:14 -0200

    included faker and blabla gems to help in the seeding

--- Gemfile
+++ Gemfile
@@ -16,10 +16,14 @@ gem 'jquery-rails'
 gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
+
 gem 'cloudinary', '1.1.7'
 gem 'attachinary', github: 'assembler/attachinary'
 gem 'jquery-fileupload-rails'
 gem 'coffee-rails'
+
+gem 'faker'
+gem 'blabla', '0.0.1'
 
 group :development, :test do
   gem 'pry-byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,38 +6,51 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Landlord.destroy_all
 
-Landlord.destroy_all
+# Property.destroy_all
 
-Property.destroy_all
+# User.destroy_all
 
-User.destroy_all
+# User.create(email:"test@test.com", password: "123123")
 
-User.create(email:"test@test.com", password: "123123")
+landlords = []
 
 
-landlord1 = [
-  {
-
-      t.string   "name"
-    t.string   "rg"
-    t.string   "cpf"
-    t.date     "birthday"
-    t.string   "martial_status"
-    t.string   "profession"
-    t.string   "phone"
-    t.string   "email"
-    t.string   "address"
-    t.string   "cep"
-    t.boolean  "deposit"
-    t.decimal  "adm_rate"
-    t.decimal  "first_rate"
-    t.string   "type"
-    t.string   "bank"
-    t.string   "agency"
-    t.string   "number"
-    t.integer  "user_id"
-
+100.times {
+  landlords << {
+    name: BlaBla::Pessoa.nome
   }
-]
+}
+
+puts landlords
+
+
+
+
+
+# landlord1 = [
+#   {
+
+#       t.string   "name"
+#     t.string   "rg"
+#     t.string   "cpf"
+#     t.date     "birthday"
+#     t.string   "martial_status"
+#     t.string   "profession"
+#     t.string   "phone"
+#     t.string   "email"
+#     t.string   "address"
+#     t.string   "cep"
+#     t.boolean  "deposit"
+#     t.decimal  "adm_rate"
+#     t.decimal  "first_rate"
+#     t.string   "type"
+#     t.string   "bank"
+#     t.string   "agency"
+#     t.string   "number"
+#     t.integer  "user_id"
+
+#   }
+# ]
 
