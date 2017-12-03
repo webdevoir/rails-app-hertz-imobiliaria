fb74abf849e184519b7b3c3384e72bb4713c4d63
Juan Couso <juan.couso@gmail.com>
2017-12-03 13:44:34 -0200

    reorganize random data into a class generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,6 @@ Property.destroy_all
 
 User.create(email:"test@test.com", password: "123123", id: 1)
 
-landlords = []
-properties = []
-property_addresses = []
-property_areas = []
-property_values = []
-
 urls = [
   'http://library.webster.edu/archives/buildings/images/webstervillageapartments.jpg',
   'https://res.cloudinary.com/apartmentlist/image/upload/t_fullsize/5b30c3a3b02ddebec2c24e66c51ba33c.jpg',
@@ -32,82 +26,106 @@ urls = [
 ]
 
 
-def rg_generator
- "#{rand(10..99).to_s}.#{rand(100..999).to_s}.#{rand(100..999).to_s}-#{rand(10..99).to_s}"
+class Generator
+
+  def rg_generator
+   "#{rand(10..99).to_s}.#{rand(100..999).to_s}.#{rand(100..999).to_s}-#{rand(10..99).to_s}"
+  end
+
+  def cep_generator
+   "#{rand(10000..99999).to_s}-#{rand(100..999).to_s}"
+  end
+
+  def landlord_genetator
+    {
+      name: BlaBla::Pessoa.nome,
+      rg: rg_generator,
+      cpf: BlaBla::CPF.formatado,
+      birthday: Faker::Date.birthday(20, 65).strftime('%d, %m, %Y'),
+      martial_status: ['solteiro', 'casado', 'separado', 'divorciado', 'viúvo'].sample,
+      profession: ['engenheiro', 'advogado', 'administrado', 'arquiteto', 'dono de empresa'].sample,
+      phone: Faker::PhoneNumber.phone_number,
+      email: Faker::Internet.email,
+      address: Faker::Address.street_address + " " + Faker::Address.secondary_address,
+      cep: cep_generator,
+      deposit: [true, false].sample,
+      adm_rate: rand(0.05..0.10).round(2),
+      first_rate: rand(0.05..0.10).round(2),
+      type: ['Conta corrente', 'Conta Pupança'].sample,
+      bank: ['Banco do Brasil', 'Bradesco', 'Itau', 'Satander' ]. sample,
+      agency: rand(1000..9999),
+      number: rand(10000..9999).to_s + "-" + rand(10..99).to_s
+    }
+  end
+
+
+  def property_generator
+    {
+      code: rand(1000...9999),
+      bussiness_type: ['venda', 'alugel'].sample,
+      property_type: ['residencial', 'comercial'].sample,
+      property_kind: ['casa', 'apartamento'].sample,
+      property_state: ['novo', 'otimo', 'bom', 'reformado'].sample,
+      position: ['norte', 'sul', 'oeste', 'leste'].sample,
+      style: ['Barroco', 'Neoclassicismo', 'Neogótico', 'Neocolonial', 'Modernista', 'Contemporâneo'].sample,
+      construction_year: rand(1930..2017),
+      bathrooms: rand(0..5),
+      rooms: rand(1..10),
+      garages: rand(0..5),
+      suites: rand(0..10),
+      description: Faker::Lorem.paragraph,
+      published: [false, true].sample,
+      conditions: Faker::Lorem.sentence
+    }
+  end
+
+
+
+  def property_address_generator
+    {
+      cep: cep_generator,
+      country: 'Brasil',
+      state: 'São Paulo',
+      city: ['São Paulo', 'São Caetano do Sul', 'São Bernardo do Campo', 'Santo Andre'].sample,
+      neighborhood: ['Barcelona', 'Boa Vista', 'Centro', 'Cerâmica' ,'Fundação', 'Jardim São Caetano', 'Mauá', 'Nova Gerty', 'Olímpico', 'Oswaldo Cruz', 'Prosperidade', 'Santa Maria', 'Santa Paula', 'Santo Antônio', 'São José'].sample,
+      street: 'Rua ' + ['Arnaldo', 'Veira', 'João Castanha', 'Aranha', 'Simões'].sample,
+      number: rand(1..9999).to_s,
+      complement: "#{[*('A'..'Z')].sample}#{rand(1..999)}",
+      condo_name: Faker::Lorem.word.capitalize
+    }
+  end
+
+  def property_area_generator
+    {
+      scale: 'm2',
+      total_area: rand(100...1500).round(2),
+      usable_area: rand(100...1500).round(2),
+      area_land:  rand(100...1500).round(2)
+    }
+  end
+
+
+  def property_value_generator
+    {
+    value: rand(1000...99999),
+    iptu: rand(300..999),
+    condo_amount: rand(300..2000),
+    rate: rand(0.1..0.2).round(2)
+    }
+  end
+
 end
 
-def cep_generator
- "#{rand(10000..99999).to_s}-#{rand(100..999).to_s}"
-end
-
-# 100.times {
-#   landlords << {
-#     name: BlaBla::Pessoa.nome,
-#     rg: rg_generator,
-#     cpf: BlaBla::CPF.formatado,
-#     birthday: Faker::Date.birthday(20, 65).strftime('%d, %m, %Y'),
-#     martial_status: ['solteiro', 'casado', 'separado', 'divorciado', 'viúvo'].sample,
-#     profession: ['engenheiro', 'advogado', 'administrado', 'arquiteto', 'dono de empresa'].sample,
-#     phone: Faker::PhoneNumber.phone_number,
-#     email: Faker::Internet.email,
-#     address: Faker::Address.street_address + " " + Faker::Address.secondary_address,
-#     cep: cep_generator,
-#     deposit: [true, false].sample,
-#     adm_rate: rand(0.05..0.10).round(2),
-#     first_rate: rand(0.05..0.10).round(2),
-#     type: ['Conta corrente', 'Conta Pupança'].sample,
-#     bank: ['Banco do Brasil', 'Bradesco', 'Itau', 'Satander' ]. sample,
-#     agency: rand(1000..9999),
-#     number: rand(10000..9999).to_s + "-" + rand(10..99).to_s,
-#     user_id: User.first.id
-#   }
-# }
-
-# properties << {
-#       code: rand(1000...9999),
-#       bussiness_type: ['venda', 'alugel'].sample,
-#       property_type: ['residencial', 'comercial'].sample,
-#       property_kind: ['casa', 'apartamento'].sample,
-#       property_state: ['novo', 'otimo', 'bom', 'reformado'].sample,
-#       position: ['norte', 'sul', 'oeste', 'leste'].sample,
-#       style: ['Barroco', 'Neoclassicismo', 'Neogótico', 'Neocolonial', 'Modernista', 'Contemporâneo'].sample,
-#       construction_year: rand(1930..2017),
-#       bathrooms: rand(0..5),
-#       rooms: rand(1..10),
-#       garages: rand(0..5),
-#       suites: rand(0..10),
-#       description: Faker::Lorem.paragraph,
-#       published: [false, true].sample,
-#       conditions: Faker::Lorem.sentence
-# }
-
-# property_addresses << {
-
-#     cep: cep_generator,
-#     country: 'Brasil',
-#     state: 'São Paulo',
-#     city: ['São Paulo', 'São Caetano do Sul', 'São Bernardo do Campo', 'Santo Andre'].sample,
-#     neighborhood: ['Barcelona', 'Boa Vista', 'Centro', 'Cerâmica' ,'Fundação', 'Jardim São Caetano', 'Mauá', 'Nova Gerty', 'Olímpico', 'Oswaldo Cruz', 'Prosperidade', 'Santa Maria', 'Santa Paula', 'Santo Antônio', 'São José'].sample,
-#     street: 'Rua ' + ['Arnaldo', 'Veira', 'João Castanha', 'Aranha', 'Simões'].sample,
-#     number: rand(1..9999).to_s,
-#     complement: "#{[*('A'..'Z')].sample}#{rand(1..999)}",
-#     condo_name: Faker::Lorem.word.capitalize
-# }
-
-# property_areas << {
-#     scale: 'm2',
-#     total_area: rand(100...1500).round(2),
-#     usable_area: rand(100...1500).round(2),
-#     area_land:  rand(100...1500).round(2)
-# }
+gen = Generator.new
 
 
-# property_values << {
-#   value: rand(1000...99999),
-#   iptu: rand(300..999),
-#   condo_amount: rand(300..2000),
-#   rate: rand(0.1..0.2).round(2)
-# }
+puts gen.landlord_genetator
+puts gen.property_generator
+puts gen.property_address_generator
+puts gen.property_area_generator
+puts gen.property_value_generator
+
+
 
 
 
