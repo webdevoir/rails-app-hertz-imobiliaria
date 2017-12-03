65f1dd87f9d0b58129fc48cd9b25de9512d8fe60
Juan Couso <juan.couso@gmail.com>
2017-12-03 11:49:34 -0200

    property seed completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ Property.destroy_all
 User.create(email:"test@test.com", password: "123123", id: 1)
 
 landlords = []
+properties = []
 
 def rg_generator
  "#{rand(10..99).to_s}.#{rand(100..999).to_s}.#{rand(100..999).to_s}-#{rand(10..99).to_s}"
@@ -16,60 +17,67 @@ def cep_generator
  "#{rand(10000..99999).to_s}-#{rand(100..999).to_s}"
 end
 
-100.times {
-  landlords << {
-    name: BlaBla::Pessoa.nome,
-    rg: rg_generator,
-    cpf: BlaBla::CPF.formatado,
-    birthday: Faker::Date.birthday(20, 65).strftime('%d, %m, %Y'),
-    martial_status: ['solteiro', 'casado', 'separado', 'divorciado', 'viúvo'].sample,
-    profession: ['engenheiro', 'advogado', 'administrado', 'arquiteto', 'dono de empresa'].sample,
-    phone: Faker::PhoneNumber.phone_number,
-    email: Faker::Internet.email,
-    address: Faker::Address.street_address + " " + Faker::Address.secondary_address,
-    cep: cep_generator,
-    deposit: [true, false].sample,
-    adm_rate: rand(0.05..0.10).round(2),
-    first_rate: rand(0.05..0.10).round(2),
-    type: ['Conta corrente', 'Conta Pupança'].sample,
-    bank: ['Banco do Brasil', 'Bradesco', 'Itau', 'Satander' ]. sample,
-    agency: rand(1000..9999),
-    number: rand(10000..9999).to_s + "-" + rand(10..99).to_s,
-    user_id: User.first.id
-  }
+# 100.times {
+#   landlords << {
+#     name: BlaBla::Pessoa.nome,
+#     rg: rg_generator,
+#     cpf: BlaBla::CPF.formatado,
+#     birthday: Faker::Date.birthday(20, 65).strftime('%d, %m, %Y'),
+#     martial_status: ['solteiro', 'casado', 'separado', 'divorciado', 'viúvo'].sample,
+#     profession: ['engenheiro', 'advogado', 'administrado', 'arquiteto', 'dono de empresa'].sample,
+#     phone: Faker::PhoneNumber.phone_number,
+#     email: Faker::Internet.email,
+#     address: Faker::Address.street_address + " " + Faker::Address.secondary_address,
+#     cep: cep_generator,
+#     deposit: [true, false].sample,
+#     adm_rate: rand(0.05..0.10).round(2),
+#     first_rate: rand(0.05..0.10).round(2),
+#     type: ['Conta corrente', 'Conta Pupança'].sample,
+#     bank: ['Banco do Brasil', 'Bradesco', 'Itau', 'Satander' ]. sample,
+#     agency: rand(1000..9999),
+#     number: rand(10000..9999).to_s + "-" + rand(10..99).to_s,
+#     user_id: User.first.id
+#   }
+# }
+
+properties << {
+      code: rand(1000...9999),
+      bussiness_type: ['venda', 'alugel'].sample,
+      property_type: ['residencial', 'comercial'].sample,
+      property_kind: ['casa', 'apartamento'].sample,
+      property_state: ['novo', 'otimo', 'bom', 'reformado'].sample,
+      position: ['norte', 'sul', 'oeste', 'leste'].sample,
+      style: ['Barroco', 'Neoclassicismo', 'Neogótico', 'Neocolonial', 'Modernista', 'Contemporâneo'].sample,
+      construction_year: rand(1930..2017),
+      bathrooms: rand(0..5),
+      rooms: rand(1..10),
+      garages: rand(0..5),
+      suites: rand(0..10),
+      description: Faker::Lorem.paragraph,
+      published: [false, true].sample,
+      conditions: Faker::Lorem.sentence
 }
 
-
-# puts User.first.id
-# puts User.first.id
-# puts landlords
+puts properties
 
 
+ # t.integer  "landlord_id"
+ #    t.integer  "code"
+ #    t.string   "bussiness_type"
+ #    t.string   "property_type"
+ #    t.string   "property_kind"
+ #    t.string   "property_state"
+ #    t.string   "position"
+ #    t.string   "style"
+ #    t.integer  "construction_year"
+ #    t.integer  "bathrooms"
+ #    t.integer  "rooms"
+ #    t.integer  "garages"
+ #    t.integer  "suites"
+ #    t.text     "description"
+ #    t.boolean  "published",         default: false
+ #    t.text     "conditions"
 
 
 
-# landlord1 = [
-#   {
-
-#       t.string   "name"
-#     t.string   "rg"
-#     t.string   "cpf"
-#     t.date     "birthday"
-#     t.string   "martial_status"
-#     t.string   "profession"
-#     t.string   "phone"
-#     t.string   "email"
-#     t.string   "address"
-#     t.string   "cep"
-#     t.boolean  "deposit"
-#     t.decimal  "adm_rate"
-#     t.decimal  "first_rate"
-#     t.string   "type"
-#     t.string   "bank"
-#     t.string   "agency"
-#     t.string   "number"
-#     t.integer  "user_id"
-
-#   }
-# ]
 
