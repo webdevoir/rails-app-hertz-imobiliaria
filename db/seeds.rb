77240d0f756f39f79ec88a1e72d1de91559825fb
Juan Couso <juan.couso@gmail.com>
2017-12-03 11:03:28 -0200

    Landlord seed completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
 
-# Landlord.destroy_all
+Landlord.destroy_all
 
-# Property.destroy_all
+Property.destroy_all
 
-# User.destroy_all
-
-# User.create(email:"test@test.com", password: "123123")
+User.create(email:"test@test.com", password: "123123", id: 1)
 
 landlords = []
 
@@ -24,26 +16,33 @@ def cep_generator
  "#{rand(10000..99999).to_s}-#{rand(100..999).to_s}"
 end
 
-# 100.times {
+100.times {
   landlords << {
-    # name: BlaBla::Pessoa.nome,
-    # rg: rg_generator,
-    # cpf: BlaBla::CPF.formatado,
-    # birthday: Faker::Date.birthday(20, 65).strftime('%d, %m, %Y'),
-    # martial_status: ['solteiro', 'casado', 'separado', 'divorciado', 'viúvo'].sample
-    # profession: ['engenheiro', 'advogado', 'administrado', 'arquiteto', 'dono de empresa'].sample,
-    # phone: Faker::PhoneNumber.phone_number,
+    name: BlaBla::Pessoa.nome,
+    rg: rg_generator,
+    cpf: BlaBla::CPF.formatado,
+    birthday: Faker::Date.birthday(20, 65).strftime('%d, %m, %Y'),
+    martial_status: ['solteiro', 'casado', 'separado', 'divorciado', 'viúvo'].sample,
+    profession: ['engenheiro', 'advogado', 'administrado', 'arquiteto', 'dono de empresa'].sample,
+    phone: Faker::PhoneNumber.phone_number,
     email: Faker::Internet.email,
     address: Faker::Address.street_address + " " + Faker::Address.secondary_address,
     cep: cep_generator,
-
+    deposit: [true, false].sample,
+    adm_rate: rand(0.05..0.10).round(2),
+    first_rate: rand(0.05..0.10).round(2),
+    type: ['Conta corrente', 'Conta Pupança'].sample,
+    bank: ['Banco do Brasil', 'Bradesco', 'Itau', 'Satander' ]. sample,
+    agency: rand(1000..9999),
+    number: rand(10000..9999).to_s + "-" + rand(10..99).to_s,
+    user_id: User.first.id
   }
-# }
+}
 
 
-
-
-puts landlords
+# puts User.first.id
+# puts User.first.id
+# puts landlords
 
 
 
