19d3ce340482b13baf6075c4d689f37028175271
Juan Couso <juan.couso@gmail.com>
2017-12-04 21:29:06 -0200

    card finish and style more to scss

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,8 +1,11 @@
 class PropertiesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :index]
+  skip_before_action :authenticate_user!, only: [:home, :index, :show]
   layout 'landing-page', only: [:home]
 
   def index
+  end
+
+  def show
   end
 
   def home
