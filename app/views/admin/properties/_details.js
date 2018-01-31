fb4f9ef901bfe73e13ec5d7a0ce561a1a5a2e55b
Juan Couso <juan.couso@gmail.com>
2018-01-31 21:58:04 -0200

    moved detail javascript to correct place

--- app/views/admin/properties/_details.js
+++
@@ -1,7 +0,0 @@
-$(document).on('submit', '#my-form', function() {
-  var detail = $('#detail').val();
-  var target = $('#test-target');
-  target.append('<p class="id">' + detail + '</p>');
-  $('#detail').val('');
-  return false;
-});
