867f47843558fe701af0f8963f264b60896c85a4
Juan Couso <juan.couso@gmail.com>
2018-02-02 19:34:27 -0200

    almost complete alert with javascript

--- app/assets/javascripts/details.js
+++ app/assets/javascripts/details.js
@@ -1,17 +1,23 @@
 function addDetail() {
   const detail = $('#detail').val();
   const target = $('#detail-form #property_id');
+  const template = interpolateTamplate(detail);
+  $('detil-alert').remove();
   if (detail === '') {
-    alert('Por favor escreva um detalhe do imovel');
+    $('#add-new-detail').append("<p class='detail-alert'>Por favor escreva um detalhe do imovel</p>");
   } else if (detail.length > 19) {
-    alert('menor que 20 c');
+    alert('menor que 20 caracteres');
   } else {
-    target.after('<div class="col-md-3 center"><div class="delete-detail detail"><span class="x-detail">x</span><label for="' + detail.toLowerCase() +'">'+ detail +'</label> <input type="hidden" name="details['+ detail.toLowerCase() + ']" id="details_'+ detail.toLowerCase() + '" value="true"></div></div>');
-    $('#detail').val('');
+    target.after(template);
   }
+  $('#detail').val('');
   return false;
 };
 
+function interpolateTamplate(detail) {
+  const template = '<div class="col-md-3 center"><div class="delete-detail detail"><span class="x-detail">x</span><label for="' + detail.toLowerCase() +'">'+ detail +'</label> <input type="hidden" name="details['+ detail.toLowerCase() + ']" id="details_'+ detail.toLowerCase() + '" value="true"></div></div>';
+  return template;
+}
 
 
 
