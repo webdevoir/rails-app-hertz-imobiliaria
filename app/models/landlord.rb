ab3f79cf68a52a4b69b5e7eb7e9014c62951217b
Juan Couso <juan.couso@gmail.com>
2018-04-02 21:56:50 +0100

    only name and telfone is required for validation

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -2,25 +2,25 @@ class Landlord < ApplicationRecord
   has_many :properties, dependent: :destroy
 
   validates :name, presence: true
-  validates :rg, presence: true
-  validates :cpf, presence: true
-  validates :birthday, presence: true
-  validates :martial_status, presence: true
-  validates :profession, presence: true
+  # validates :rg, presence: true
+  # validates :cpf, presence: true
+  # validates :birthday, presence: true
+  # validates :martial_status, presence: true
+  # validates :profession, presence: true
   validates :phone1, presence: true
-  validates :email, presence: true
-  validates :cep, presence: true
-  validates :state, presence: true
-  validates :city, presence: true
-  validates :neighborhood, presence: true
-  validates :street, presence: true
-  validates :number, presence: true
-  # validates :deposit, presence: true
-  validates :adm_rate, presence: true
-  validates :first_rate, presence: true
-  validates :type_of_account, presence: true
-  validates :bank, presence: true
-  validates :agency, presence: true
+  # # validates :email, presence: true
+  # # validates :cep, presence: true
+  # # validates :state, presence: true
+  # # validates :city, presence: true
+  # # validates :neighborhood, presence: true
+  # # validates :street, presence: true
+  # # validates :number, presence: true
+  # # validates :deposit, presence: true
+  # validates :adm_rate, presence: true
+  # validates :first_rate, presence: true
+  # validates :type_of_account, presence: true
+  # validates :bank, presence: true
+  # validates :agency, presence: true
 
   def self.search(query)
     sql_query = " \
