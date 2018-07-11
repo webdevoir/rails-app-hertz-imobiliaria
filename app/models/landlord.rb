0df3ec238e0f15f4bddd74a19d799150c92ce1b0
Juan Couso <juan.couso@gmail.com>
2018-07-11 13:36:51 +0100

    fix issue with validations

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -10,13 +10,13 @@ class Landlord < ApplicationRecord
   validates :phone1, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Use um email valido"},
                     uniqueness: { message: "Voce já usou esse email"},
-                    length: {minimum: 5, maximum: 25}
+                    length: {minimum: 5, maximum: 25}, allow_blank: true
   # # validates :cep, presence: true
   # # validates :state, presence: true
   # # validates :city, presence: true
   # # validates :neighborhood, presence: true
   # # validates :street, presence: true
-  validates :number, numericality: { only_integer: true }
+  # validates :number, numericality: { only_integer: true }
   # # validates :deposit, presence: true
   # validates :adm_rate, presence: true
   # validates :first_rate, presence: true
